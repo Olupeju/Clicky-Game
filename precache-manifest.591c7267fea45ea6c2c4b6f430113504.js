fde4de64b6ae2bff9f97c19c352cfba3cbee3a40
unknown <olayika@hotmail.com>
2019-02-23 10:11:05 -0600

    Updates

---
+++ precache-manifest.591c7267fea45ea6c2c4b6f430113504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ecdbd8bcf55b4f97a74b",
+    "url": "/static/js/main.ecdbd8bc.chunk.js"
+  },
+  {
+    "revision": "34ad5c2a094df235dc73",
+    "url": "/static/js/2.34ad5c2a.chunk.js"
+  },
+  {
+    "revision": "ecdbd8bcf55b4f97a74b",
+    "url": "/static/css/main.204e6415.chunk.css"
+  },
+  {
+    "revision": "f03737601012a26927605190accf2122",
+    "url": "/index.html"
+  }
+];
